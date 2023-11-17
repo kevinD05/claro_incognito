2c2645415cbbe3c756002a68fe389c53665de776
kevinD05 <alexanderkevin05@gmail.com>
2023-11-17 14:00:05 -0600

    fix step

--- features/steps/claro.py
+++ features/steps/claro.py
@@ -33,28 +33,33 @@ def step_impl(context):
     driver = context.browser
     driver.execute_script('window.scrollTo(0, document.documentElement.scrollHeight / 1.3);')
     time.sleep(2)
-    firs_name = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[1]/label')
+    driver.switch_to.frame(driver.find_element(By.XPATH, '//iframe[@id="form-usclaro"]'))
+    time.sleep(2)
+    firs_name = driver.find_element(By.XPATH, '//input[@name="1005782_419038pi_1005782_419038"]')
     firs_name.click()
     firs_name.send_keys('kevin')
     time.sleep(3)
-    last_name = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[2]/label')
+    last_name = driver.find_element(By.XPATH, '//input[@name="1005782_419041pi_1005782_419041"]')
     last_name.click()
     last_name.send_keys('diaz')
     time.sleep(2)
-    company_name = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[3]/label')
+    company_name = driver.find_element(By.XPATH, '//input[@name="1005782_419044pi_1005782_419044"]')
     company_name.click()
     company_name.send_keys('chester enterprise')
     time.sleep(1)
-    email = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[4]/label')
+    email = driver.find_element(By.XPATH, '//input[@name="1005782_419047pi_1005782_419047"]')
     email.click()
     email.send_keys('kevin@gmail.com')
     time.sleep(1)
-    phone_number = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[5]/label')
+    phone_number = driver.find_element(By.XPATH, '//input[@name="1005782_419050pi_1005782_419050"]')
     phone_number.click()
     phone_number.send_keys('88523793')
     time.sleep(1)
-    interested = driver.find_element(By.XPATH, '//*[@id="pardot-form"]/p[5]/label')
+    interested = driver.find_element(By.XPATH, '//select[@name="1005782_419053pi_1005782_419053"]')
     interested.click()
+    time.sleep(1)
+    business = driver.find_element(By.XPATH, '//option[@value="2541394"]')
+    business.click()
     time.sleep(1)
 
 
@@ -65,5 +70,6 @@ def step_impl(context):
 
 @step(u'validamos mensaje de error')
 def step_impl(context): 
-    error_message = context.browser.find_element(By.XPATH, '//*[@id="pardot-form"]/p[15]')  
-    assert "Mensaje de error esperado" in error_message.text, "El mensaje de error no coincide con lo esperado"
+    error_message = context.browser.find_element(By.XPATH, '//p[text()="Invalid CAPTCHA"]')  
+    assert "Mensaje de error esperado" in error_message.text, "El mensaje de error coincide con lo esperado"
+    time.sleep(2)
